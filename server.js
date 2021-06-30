50fd98793deb3b204cf1ccd996001995628e3fa6
ammarRBK <alshakmaskeain@gmail.com>
2021-06-30 14:51:37 +0300

    remove less-used two functions from server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
@@ -20,30 +19,17 @@ app.use(session({
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
-app.use(express.static(path.join(__dirname, 'melody-life-server')));
+app.use(express.static(path.join(__dirname, 'life-melody-server')));
 
 
 // app.use('/api/tasks', tasks);
 
+app.listen('3000',()=>{
+  console.log("app is ready")
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
+app.get('/api',(req,res)=>{
+  res.send({message:"hello Ammar"})
+})
 
 module.exports = app;
