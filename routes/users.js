c872ec17d6e13bc062b6951a8f9e0a9ec6630026
ammarRBK <alshakmaskeain@gmail.com>
2021-07-14 11:26:14 +0300

    adding rows to users database table from server worked fine

--- routes/users.js
+++ routes/users.js
@@ -18,32 +18,34 @@ router.post('/signup',(req,res)=>{
     let userName= req.body.userName;
     let password= req.body.password;
     let phoneNumber= req.body.phoneNumber;
-    let adress= req.body.adress || null;
-
+    let address= req.body.address;
+    
     bcrypt.hash(password,15)
     .then((hashedPassword)=>{
         var newUser = {
             userName: userName,
             password: hashedPassword,
             phoneNumber: phoneNumber,
-            adress: adress
+            address: address
         }
+
         console.log(newUser);
+        db.create(newUser).then(()=>{
+            console.log("-------->done")
+            
+        })
         // var user = new db(newUser);
         // user.save()
         // .then(item => {
         //     res.send("user saved in database");
-        })
+        // })
     })
     .then(()=>{
-        
-        res.send("User saved with hashed password");
+        res.send({successMessage:"User saved with hashed password"});
     })
     .catch(error =>{
         res.send(error);
     })
-
-    res.send({message:"hello Ammar from users"})
-})
+});
 
 module.exports= {router};
