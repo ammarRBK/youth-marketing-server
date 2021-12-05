f3cbda3d71cf71129123e5e16ad1ddc5c276eb7b
ammarRBK <alshakmaskeain@gmail.com>
2021-12-05 13:24:29 +0200

    update database connection to be online instead of locally

--- database/dbCon.js
+++ database/dbCon.js
@@ -1,7 +1,7 @@
 const { Sequelize }= require("sequelize");
 
-const sequelize= new Sequelize("youthmarketing", "root", "ammar@1234",{
-    host: 'localhost',
+const sequelize= new Sequelize("youthmarketing", "ammar", "ammar@1234",{
+    host: 'mysql-60776-0.cloudclusters.net',
     dialect: 'mysql'
 });
 
