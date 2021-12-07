f2fc866b66ba2dd26c39e19d7110fe0cb448b044
ammarRBK <alshakmaskeain@gmail.com>
2021-12-07 14:23:03 +0200

    trying

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var path = require('path');
 var fs= require('fs');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var cors= require('cors')
-app.use(cors({
-  "Access-Control-Allow-Origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "preflightContinue": false,
-  "optionsSuccessStatus": 204
-}))
+// var cors= require('cors')
+// app.use(cors({
+//   "Access-Control-Allow-Origin": "*",
+//   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   "preflightContinue": false,
+//   "optionsSuccessStatus": 204
+// }))
 
 var products = require('./routes/products').router;
 var users= require('./routes/users').router;
@@ -33,6 +33,26 @@ app.use(express.static(path.join(__dirname, './life-melody-server')));
 
 app.use('/api/users', users);
 app.use('/api/products',products);
+
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.listen(process.env.PORT ||'3000',()=>{
   console.log("app is ready")
