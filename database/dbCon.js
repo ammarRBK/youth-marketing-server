c0ab6e32895aea06604b7535738c6657a03b2b17
ammarRBK <alshakmaskeain@gmail.com>
2022-01-22 22:36:59 +0200

    changing database

--- database/dbCon.js
+++ database/dbCon.js
@@ -1,8 +1,8 @@
 const { Sequelize }= require("sequelize");
 
 const sequelize= new Sequelize("youthmarketing", "ammar", "ammar@1234",{
-    host: 'mysql-65187-0.cloudclusters.net',
-    port: '17010',
+    host: 'mysql-66942-0.cloudclusters.net',
+    port: '10149',
     dialect: 'mysql'
 });
 
