3f20c88c7ae25d5b25b0f43206eeebee78cc9bd5
ammarRBK <alshakmaskeain@gmail.com>
2022-02-08 13:08:48 +0200

    try to dend ready image to client

--- routes/products.js
+++ routes/products.js
@@ -111,7 +111,7 @@ router.get('/getproducts',cors(corsOptions),(req,res)=>{
       // let imagedata= JSON.stringify(product.image)
         // let BetoA= btoa(imagedata.data.reduce((data, byte) => data + String.fromCharCode(byte), ''));
 
-        productObj.image= Buffer.isBuffer(product.image);
+        productObj.image= JSON.parse(JSON.stringify(product.image));
         // 'data:image/jpeg;base64,' + hexToBase64(product.image.toString('binary'))
         // `data:image/jpeg;base64,${product.image.toString('base64')}`;
         // `data:image/jpeg;base64,${Buffer.from(JSON.parse(imagedata)).toString("base64")}`;
