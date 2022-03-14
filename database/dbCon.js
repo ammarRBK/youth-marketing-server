c4fba5065a9f4de8c9a633c15c69e3ab77506a73
ammarRBK <alshakmaskeain@gmail.com>
2022-03-14 13:17:13 +0200

    updating database connection parameters

--- database/dbCon.js
+++ database/dbCon.js
@@ -1,8 +1,8 @@
 const { Sequelize }= require("sequelize");
 
 const sequelize= new Sequelize("youthmarketing", "ammar", "ammar@1234",{
-    host: 'mysql-67945-0.cloudclusters.net',
-    port: '18213',
+    host: 'mysql-72075-0.cloudclusters.net',
+    port: '18794',
     dialect: 'mysql',
     logging: false
 });
