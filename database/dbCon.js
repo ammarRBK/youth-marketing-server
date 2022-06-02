0f9b8b72f39d7ac7f9396542aca86e05c29a5943
ammarRBK <alshakmaskeain@gmail.com>
2022-06-02 21:34:51 +0300

    change database connection hostname and port

--- database/dbCon.js
+++ database/dbCon.js
@@ -1,8 +1,8 @@
 const { Sequelize }= require("sequelize");
 
 const sequelize= new Sequelize("youthmarketing", "ammar", "ammar@1234",{
-    host: 'mysql-78972-0.cloudclusters.net',
-    port: '17182',
+    host: 'mysql-79695-0.cloudclusters.net',
+    port: '18802',
     dialect: 'mysql',
     logging: false
 });
