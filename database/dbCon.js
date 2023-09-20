abb3a8c3b245958d3d6a361b831aee0cd06c17a1
ammarRBK <alshakmaskeain@gmail.com>
2023-09-20 12:31:18 +0300

    database connection server changer to filess.io

--- database/dbCon.js
+++ database/dbCon.js
@@ -1,8 +1,8 @@
 const { Sequelize }= require("sequelize");
 
-const sequelize= new Sequelize("sql12645429", "sql12645429", "x6sXFRtysd",{
-    host: 'sql12.freemysqlhosting.net',
-    port: '3306',
+const sequelize= new Sequelize("youthmarketing_teafrozen", "youthmarketing_teafrozen", "b66c205eaae4a51e5c904c3626c6e118fd1177c8",{
+    host: '65t.h.filess.io',
+    port: '3307',
     dialect: 'mysql',
     logging: false
 });
