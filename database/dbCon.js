9b00a115a05d92b03cd9bf1a0cc6037eaa121d2c
ammarRBK <alshakmaskeain@gmail.com>
2023-12-03 09:11:05 +0300

    change databace connection argumants

--- database/dbCon.js
+++ database/dbCon.js
@@ -1,7 +1,7 @@
 const { Sequelize }= require("sequelize");
 
-const sequelize= new Sequelize("youthmarketing_teafrozen", "youthmarketing_teafrozen", "b66c205eaae4a51e5c904c3626c6e118fd1177c8",{
-    host: '65t.h.filess.io',
+const sequelize= new Sequelize("youthmarketing_hourpupil", "youthmarketing_hourpupil", "5221a3e52a84f8ee968dfb5fd6539ee36154b091",{
+    host: '8ax.h.filess.io',
     port: '3307',
     dialect: 'mysql',
     logging: false
